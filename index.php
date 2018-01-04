9aad6fca3e56190f61d1436563f4d67577cab995
tanvir hasan <tanvir002700@gmail.com>
2018-01-04 21:29:26 +0600

    successfully connect db with pdo

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-    echo "initialize project";
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=php_learn', 'root', 'root');
+    } catch (PDOException $e) {
+        var_dump($e);
+    }
