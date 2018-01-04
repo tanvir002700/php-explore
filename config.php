6a1e288e7b4d472b22d21cb17fd73762d0fff6cc
tanvir hasan <tanvir002700@gmail.com>
2018-01-05 00:10:27 +0600

    add secrate database config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'php_learn',
+        'username' => 'root',
+        'password' => 'root',
+        'connection' => 'mysql:host=localhost',
+        'options' => []
+    ]
+];
