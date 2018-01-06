257bfaf33c33d08841a59902425fbf515d5665e2
tanvir hasan <tanvir002700@gmail.com>
2018-01-07 00:11:59 +0600

    add request class

--- core/Initializer.php
+++ core/Initializer.php
@@ -2,6 +2,7 @@
 
 $config = require 'config.php';
 require 'core/Router.php';
+require 'core/Request.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuilder.php';
 
--- index.php
+++ index.php
@@ -7,9 +7,5 @@ $router = new Router();
 
 require 'routes.php';
 
-//die(var_dump($_SERVER));
-
-$uri = trim($_SERVER['REQUEST_URI'], '/');
-
 require $router::load('routes.php')
-    -> direct($uri);
+    -> direct(Request::uri());
