1df4a3cbc6532727bd0069a1efa3ad62e841ec30
tanvir hasan <tanvir002700@gmail.com>
2018-01-17 21:45:13 +0600

    refactor routes

--- core/Router.php
+++ core/Router.php
@@ -23,8 +23,17 @@ class Router{
 
     public function direct($uri, $requestType) {
         if(array_key_exists($uri, $this->routes[$requestType])) {
-            return $this->routes[$requestType][$uri];
+            return $this->callAction(
+                ...explode('@', $this->routes[$requestType][$uri])
+            );
         }
         throw new Exception('No route defined for this URI.');
+    }
+
+    protected function callAction($controller, $action) {
+        if(! method_exists($controller, @$action)) {
+            throw new Exception('Controller method not responding.');
+        }
+        return (new $controller)->$action();
     }
 }
