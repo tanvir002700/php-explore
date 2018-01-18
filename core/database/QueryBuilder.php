15b81c09c9b0cc4897d8affe49fe41be850f53d1
tanvir hasan <tanvir002700@gmail.com>
2018-01-18 17:10:06 +0600

    add new query builder

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -32,4 +32,10 @@ class QueryBuilder {
             throw new \Exception('Something went wrong');
         }
     }
+
+    public function find($table, $id) {
+        $statement = $this->pdo->prepare("SELECT * FROM {$table} WHERE id={$id}");
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_CLASS);
+    }
 }
